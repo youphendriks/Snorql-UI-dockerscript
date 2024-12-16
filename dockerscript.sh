c8ed8e7f198017e74b4aecbcffa257cac91ad2c8
youphendriks <youp@youphendriks>
2024-12-16 12:38:41 +0100

    Edit dockerscript

--- dockerscript.sh
+++ dockerscript.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-#
-#Ask username
-echo Hello, what is your username?
-read varname
-echo Your username is set to: $varname
 #Clone Snorql-UI
 echo Now cloning the Snorql-UI git
 git clone git@github.com:wikipathways/Snorql-UI.git
@@ -11,9 +6,8 @@ git clone git@github.com:wikipathways/Snorql-UI.git
 echo Creating some directories
 mkdir db
 mkdir db/database
-#Change paths to use correct username
-echo Now adjusting the filepaths in docker-compose.yml to include your username
-sed 's/{username}/'$varname'/g' docker-compose-template.yml > docker-compose.yml
+# Remove running instances
+docker remove wikipathways-virtuoso wikipathways-snorql
 #Run the docker
 echo Starting the docker image...
 docker compose up -d
